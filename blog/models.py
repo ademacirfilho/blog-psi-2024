8e70d2709acae297df0b84ef18b2089e2c064b17
Ademacir Filho <ademacirfilho@gmail.com>
2024-09-13 08:21:41 -0300

    ajustes no codigo

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(max_length=500)
-    imagem = models.ImageField(blank=True, upload_to='media/')
+    imagem = models.ImageField(blank=True)
 
     def __str__(self):
         return self.titulo
@@ -12,7 +12,7 @@ class Noticia(models.Model):
     titulo = models.CharField(max_length=100)
     data = models.DateField()
     url = models.URLField(max_length=200)
-    imagem = models.ImageField(blank=True, upload_to='media/')
+    imagem = models.ImageField(blank=True)
 
     def __str__(self):
         return self.titulo
